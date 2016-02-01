6c44854487081782352a0d223a2fd6783a3391ee
niuyan <632176758@qq.com>
2016-02-02 00:58:34 +0800

    commit

---
+++ CalendarTodoList/calendarDay/TodoDetail/TodoProjectView.m
@@ -0,0 +1,39 @@
+//
+//  TodoProjectView.m
+//  CalendarTodoList
+//
+//  Created by 牛严 on 16/2/2.
+//  Copyright © 2016年 牛严. All rights reserved.
+//
+
+#import "TodoProjectView.h"
+
+@implementation TodoProjectView
+{
+    UILabel *_contentLabel;
+}
+
+#pragma mark 初始化
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.backgroundColor = [UIColor whiteColor];
+        [self initViews];
+    }
+    return self;
+}
+
+- (void)initViews
+{
+    UILabel *titleLabel  = [[UILabel alloc]init];
+    titleLabel.text = @"项目组";
+    titleLabel.font = [UIFont boldSystemFontOfSize:15];
+    [self addSubview:titleLabel];
+    [titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self).offset(25);
+        make.centerY.equalTo(self);
+    }];
+}
+
+@end
